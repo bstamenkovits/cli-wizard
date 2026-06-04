3531750b2f07e54c8c5bda3657e963eafca8bfe6
Bela <brstamenkovits@gmail.com>
2026-06-04 22:44:21 +0900

    Add comment clarifying GEMINI_API_KEY in example configuration

--- src/cli_wizard/core/gemini_interface.py
+++ src/cli_wizard/core/gemini_interface.py
@@ -86,7 +86,7 @@ class GeminiInterface:
 
 
 if __name__ == "__main__":
-    config = {'GEMINI_API_KEY': "***REMOVED***"}
+    config = {'GEMINI_API_KEY': "***REMOVED***"}  # not active key
     gem = GeminiInterface(config)
     output = gem.generate_command("ls -a")
 
