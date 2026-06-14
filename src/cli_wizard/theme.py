318bdbbec8afcc010f235a9a69824c7165a11abe
Bela <brstamenkovits@gmail.com>
2026-06-14 10:54:22 +0700

    use rich and questionary for config editing and display

---
+++ src/cli_wizard/theme.py
@@ -0,0 +1,27 @@
+import questionary
+
+
+class Style:
+    def __init__(self, color:str, weight:str = ""):
+        self.color = color
+        self.weight = weight
+
+    @property
+    def rich_style(self):
+        return f"fg:{self.color} {self.weight}"
+
+    @property
+    def questionary_style(self):
+        return f"fg:{self.color} {self.weight}"
+
+
+class THEME:
+    primary = Style(
+        color="#ff9d00",
+        weight="bold",
+    )
+
+
+custom_questionary_style = questionary.Style([
+    ('highlighted', f'fg:{THEME.primary.color} {THEME.primary.weight}'), # pointed-at choice in select and checkbox prompts
+])
